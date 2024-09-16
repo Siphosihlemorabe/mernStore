c68bd0f4e99579e2970bbbf91db7879a0c9bd156
Siphosihlemorabe <siphosim9@gmail.com>
2024-09-16 17:24:26 +0200

    Updated MongoDB IP whitelist

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/api/products",productRoutes);
 const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"/fullstack-store/frontend/dist")));
+  app.use(express.static(path.join(__dirname,"/frontend/dist")));
   
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
